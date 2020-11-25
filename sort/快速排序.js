3be7e185ceecfe0055f4791c5647624d60e2a881
yinhaiying <yinhaiying@58.com>
2020-11-25 10:59:58 +0800

    交换位置实现获取快速排序的排序

--- sort/快速排序.js
+++ sort/快速排序.js
@@ -7,6 +7,7 @@ const quickSort = (arr) => {
     ]
 }
 
+// 数学思维实现方式二
 const quickSort2 = (arr) => {
     if(arr.length <=1){
         return arr;
@@ -31,5 +32,82 @@ const quickSort2 = (arr) => {
 
 
 */
-console.log(quickSort([1,5,2,6,3,4]));  // [1,2,3,4,5,6]
-console.log(quickSort2([1,5,2,6,3,4]));  // [1,2,3,4,5,6]
+// console.log(quickSort([1,5,2,6,3,4]));  // [1,2,3,4,5,6]
+// console.log(quickSort2([1,5,2,6,3,4]));  // [1,2,3,4,5,6]
+
+
+// 小的往左走，大的往右走。
+/* 
+需求：
+数组：arr=[40,70,20,10,60,50,30]
+函数handlerPivot以40为基准
+小的往左走，大的往右走
+使得数组arr变成[...小于40的数,40,...大于40的数]
+返回值为povit的新的下标
+不能创建新数组，只能就地操作
+
+*/
+
+// 不使用新的数组。
+let inPlaceQuickSort = (arr) => {
+    let pivot = arr[0];
+    let index = 0;
+    let i = 1;
+    let w = 0;
+    while(i < arr.length){
+      let w = 0;
+      while(arr[i] >= pivot){
+          i+=1;
+      }
+      while(arr[i] < pivot){
+        w+=1;
+        i+=1;
+      }
+      let part = arr.splice(i-w,w);
+      arr.splice(index,0,...part);
+      index += w;
+    }
+    return [arr,index]
+}
+
+
+/* 
+上面的代码中，虽然我们没有使用新的数组，但是使用splice进行切割时，数组中元素的位置，
+其实是不断在移动地，因此如果splice次数非常多，那么移动的次数就很多了。那么我们可不可以换个想法，
+不移动，只是替换，这样的话就没有太多的移动操作了。
+
+*/
+// let arr = [40, 20, 10, 70, 60, 50, 30];
+// const [array, index] = inPlaceQuickSort(arr);
+// console.log("arr:", array);
+// console.log("index:", index);
+
+
+// 不使用splice进行移动，而是通过替换
+
+let arr = [40, 70,60,20, 10, 30, 50];
+let arr1 = [40, 20, 10, 30, 50,60,70];
+let arr2 = [40, 20, 20, 30, 20,30,50];
+// 获取到pivot最终的位置
+const handlePivot = (arr) => {
+    let pivot = arr[0];
+    let i = 1;
+    let j = 1;
+    let len = arr.length;
+    while( (i + j) < (len-1) ){
+      while(arr[i] > pivot && (i+j) < len-1){
+        [arr[i],arr[len-j]] = [arr[len-j],arr[i]];
+        j += 1;
+      }
+      while(arr[i] <= pivot && i < len){
+          i++;
+      }
+    }
+    return --i;
+}
+
+console.log(handlePivot(arr));
+console.log(handlePivot(arr1));
+console.log(handlePivot(arr2));
+
+
